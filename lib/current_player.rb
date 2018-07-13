70e0b6267b948d59eca45e22f7e903ae347a5f3c
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-13 00:58:26 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,19 @@
-
 board = Array.new(9, "")
 
 def turn_count(board)
-  the_amount_of_turns = 0
+  number_of_turns = 0
   board.each do |el|
     if el == "X" || el == "O"
-      the_amount_of_turns += 1
+      number_of_turns += 1
     end
   end
-  the_amount_of_turns
+  number_of_turns
 end
-def current_player(board, turn_count)
-    if turn_count(board) % 2 == 0
-      return "X"
-    else turn_count(board) % 2 == 1
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else turn_count(board) % 2 == 1
       return "O"
     end
-end
-#x is odd, o is even
+  end
